18b9cf7ef13a2d570a0fdb85af70c9783b6d8496
Thomas Beeley <tbeeley@gmail.com>
2014-10-19 11:32:28 +0100

    match started

---
+++ lib/match.rb
@@ -0,0 +1,10 @@
+class Match
+
+	def initialize(hometeam, awayteam)
+		@hometeam = hometeam
+		@awayteam = awayteam
+	end
+
+	attr_accessor :hometeam, :awayteam
+
+end
---
+++ spec/match_spec.rb
@@ -0,0 +1,20 @@
+require 'match'
+
+describe Match do
+
+	context 'before kickoff' do
+
+		let(:match) { Match.new(team1, team2)}
+		let(:team1) {double :team}
+		let(:team2) {double :team}
+
+
+		it 'should a home team' do
+			expect(match.hometeam).to eq(team1)
+		end
+
+		it 'should have an away team' do
+			expect(match.awayteam).to eq(team2)
+		end
+	end
+end
