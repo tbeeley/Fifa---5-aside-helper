9ae13d44d9f095249e6bc820210103c21a9d60a0
Thomas Beeley <tbeeley@gmail.com>
2014-10-19 13:45:25 +0100

    working on managing squad

--- lib/team.rb
+++ lib/team.rb
@@ -3,6 +3,7 @@ class Team
 	def initialize(name, manager, stadium)
 		@manager = manager
 		@stadium = stadium
+		# For firsts do an array x 11 like in SUdoku (instead of capacity?)
 		@firsts = []
 		@subs = []
 		@rating = 50
@@ -14,5 +15,13 @@ class Team
 	def change_rating(new_rating)
 		@rating = new_rating
 	end
+
+	def add_player_to_firsts(player)
+		unless firsts.?
+			@firsts << player
+		end
+	end
+
+
 
 end
--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -5,19 +5,12 @@ describe Team do
 	let(:team) { Team.new('Chelsea', manager, stadium) }
 	let(:manager) { double :manager }
 	let(:stadium) { double :stadium }
+	let(:player)  { double :player  }
 
-	context 'when created' do
+	context 'when initialized' do
 
 		it 'should have a name' do
 			expect(team.name).to eq 'Chelsea'
-		end
-
-		it 'should have a starting eleven' do
-			expect(team.firsts).to eq []
-		end
-
-		it 'should have a subs bench' do
-			expect(team.subs).to eq []
 		end
 
 		it 'should have a manager' do
@@ -30,6 +23,30 @@ describe Team do
 
 		it 'should have a default rating of 50' do
 			expect(team.rating).to eq 50
+		end
+
+	end
+
+	context 'team management' do
+
+		it 'should be able to manually change rating' do
+			expect(team.rating).to eq 50
+			team.change_rating(100)
+			expect(team.rating).to eq 100
+		end
+
+		it 'should have a starting eleven' do
+			expect(team.firsts).to eq []
+		end
+
+		it 'should have a subs bench' do
+			expect(team.subs).to eq []
+		end
+
+		it 'should be able to add a player to the firsts' do
+			expect(team.firsts.count).to eq 0
+			team.add_player_to_firsts(player)
+			expect(team.firsts.count).to eq 1
 		end
 
 	end
