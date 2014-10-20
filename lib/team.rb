c2b01b2172cab88a2852e1586e4996b505fe791a
Thomas Beeley <tbeeley@gmail.com>
2014-10-20 15:46:57 +0100

    moving players complete, now to refactor

--- lib/team.rb
+++ lib/team.rb
@@ -3,15 +3,15 @@ class Team
 	def initialize(name, manager, stadium)
 		@manager = manager
 		@stadium = stadium
-		# For firsts do an array x 11 like in SUdoku (instead of capacity?)
-		@firsts = []
+		# For first_team do an array x 11 like in SUdoku (instead of capacity?)
+		@first_team = []
 		@subs = []
 		@squad = []
 		@rating = 50
 		@name = name
 	end
 
-	attr_accessor :firsts, :subs, :manager, :stadium, :rating, :name, :squad
+	attr_accessor :first_team, :subs, :manager, :stadium, :rating, :name, :squad
 
 	def change_rating(new_rating)
 		@rating = new_rating
@@ -21,16 +21,48 @@ class Team
 		squad << Player.new(name, rating)
 	end
 
-	def full?
-		firsts.count == 11
-	end
-
 	#Have one central method for upgrading, one for demoting. with addplayertofirsts and another in the middle.
 
-	def add_player_to_firsts(player)
-		unless firsts.full?
-			@firsts << player
+	def promote(player)
+		remove_from_subs(player)
+		add_to_first_team(player)
+	end
+
+	def demote(player)
+		remove_from_first_team(player)
+		add_to_subs(player)
+	end
+
+	def add_to(team_type, player)
+		if team_type.count == 11
+			raise 'you already have 11 players'
+		else
+			team_type << player
 		end
+	end
+
+	def add_to_first_team(player)
+		if first_team.count == 11
+			raise 'you already have 11 players'
+		else
+			@first_team << player
+		end
+	end
+
+	def remove_from_first_team(player)
+		@first_team.delete(player)
+	end
+
+	def add_to_subs(player)
+		if subs.count == 7
+			raise 'your subs bench is full'
+		else
+			@subs << player
+		end
+	end
+
+	def remove_from_subs(player)
+		@subs.delete(player)
 	end
 
 
--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -25,10 +25,6 @@ describe Team do
 			expect(team.rating).to eq 50
 		end
 
-		it 'should have a squard' do
-			expect(team.squad).to eq []
-		end
-
 	end
 
 	context 'team management' do
@@ -40,23 +36,60 @@ describe Team do
 		end
 
 		it 'should have a starting eleven' do
-			expect(team.firsts).to eq []
+			expect(team.first_team).to eq []
 		end
 
 		it 'should have a subs bench' do
 			expect(team.subs).to eq []
 		end
 
-		it 'should be able to create a player' do
+		it 'should have a squard' do
+			expect(team.squad).to eq []
+		end
+
+	end
+
+	context 'creating and organising players' do
+
+		it 'should be able to create a player and put in squad' do
 			expect(team.squad.count).to eq 0
 			team.create_player('Andrea Pirlo', 88)
 			expect(team.squad.count).to eq 1
 		end
 
 		it 'should be able to add a player to the firsts' do
-			expect(team.firsts.count).to eq 0
-			team.add_player_to_firsts(player)
-			expect(team.firsts.count).to eq 1
+			expect(team.first_team.count).to eq 0
+			team.add_to_first_team(player)
+			expect(team.first_team.count).to eq 1
+		end
+
+		it 'should limit the first_team to 11 players' do
+			expect(team.first_team.count).to eq 0
+			expect( lambda { 12.times {team.add_to_first_team(player) }}).to raise_error 'you already have 11 players'
+		end
+
+		it 'should be able to add a player to the subs' do
+			expect(team.subs.count).to eq 0
+			team.add_to_subs(player)
+			expect(team.subs.count).to eq 1
+		end
+
+		it 'should be able to promote players to the first_team' do
+			team.add_to_subs(player)
+			expect(team.first_team.count).to eq 0
+			expect(team.subs.count).to eq 1
+			team.promote(player)
+			expect(team.first_team.count).to eq 1
+			expect(team.subs.count).to eq 0
+		end
+
+		it 'should be able to demote players to the subs' do
+			team.add_to_first_team(player)
+			expect(team.subs.count).to eq 0
+			expect(team.first_team.count).to eq 1
+			team.demote(player)
+			expect(team.subs.count).to eq 1
+			expect(team.first_team.count).to eq 0
 		end
 
 	end
